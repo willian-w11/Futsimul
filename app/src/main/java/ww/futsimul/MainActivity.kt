a777899edd27f1612fa770e8db3a2c8889336c37
willian-w11 <w.wesley11@gmail.com>
2022-08-05 00:58:52 -0300

    foi add a função buildfeatures.

--- app/src/main/java/ww/futsimul/MainActivity.kt
+++ app/src/main/java/ww/futsimul/MainActivity.kt
@@ -1,11 +1,22 @@
 package ww.futsimul
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import ww.futsimul.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+    //setContentView(R.layout.activity_main)
+       //val tvHello = findViewById<TextView>(R.id.tvHello)
+
+       binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.tvHello.text = "É gooooolll"
     }
 }
